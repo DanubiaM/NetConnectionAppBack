95eb15469f0c9e39a31517ce33cc536feafb54ef
Danubia Macedo <dubamacedoo@gmail.com>
2024-02-18 19:37:55 -0400

    feat: new functions

--- src/test/java/netconnection/app/back/FakeClientServicePort.java
+++ src/test/java/netconnection/app/back/FakeClientServicePort.java
@@ -1,14 +1,27 @@
 package netconnection.app.back;
 
 import netconnection.app.back.application.dtos.ClientDTO;
+import netconnection.app.back.domain.model.Client;
 import netconnection.app.back.domain.port.ClientServicePort;
 
 import java.util.List;
 
 public class FakeClientServicePort implements ClientServicePort {
     @Override
-    public void saveClient(ClientDTO clientDTO) {
+    public Client save(Client clientDTO) {
         //empty fake implementation
+
+        return new Client();
+    }
+
+    @Override
+    public Client findById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return null;
     }
 
 }
