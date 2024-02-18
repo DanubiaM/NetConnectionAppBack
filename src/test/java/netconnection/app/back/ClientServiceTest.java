d9f9bfdb9ff68e703afa2480df85a3a06940844a
Danubia Macedo <dubamacedoo@gmail.com>
2024-02-18 19:35:22 -0400

    refactor: rename class

---
+++ src/test/java/netconnection/app/back/ClientServiceTest.java
@@ -0,0 +1,22 @@
+package netconnection.app.back;
+
+import netconnection.app.back.domain.model.Address;
+import netconnection.app.back.domain.model.CPF;
+import netconnection.app.back.domain.model.Client;
+import netconnection.app.back.domain.service.ClientServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientServiceTest {
+}
