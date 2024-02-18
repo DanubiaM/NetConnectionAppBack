4e259b2e2cfe8664eeb7e008f0ddd3c5d09e708b
Danubia Macedo <dubamacedoo@gmail.com>
2024-02-18 19:35:58 -0400

    feat: new methods

--- src/main/java/netconnection/app/back/domain/port/ClientServicePort.java
+++ src/main/java/netconnection/app/back/domain/port/ClientServicePort.java
@@ -1,6 +1,7 @@
 package netconnection.app.back.domain.port;
 
 import netconnection.app.back.application.dtos.ClientDTO;
+import netconnection.app.back.domain.model.Client;
 
 import java.util.List;
 
@@ -8,5 +9,8 @@ public interface ClientServicePort {
 
     Client save(Client clientDTO);
 
+    Client findById(String id);
+
+    List<Client> findAll();
 
 }
