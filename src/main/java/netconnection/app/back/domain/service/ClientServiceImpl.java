26035e01436ea41c3b4f8262d3f8987fef16f72b
Danubia Macedo <dubamacedoo@gmail.com>
2024-02-18 19:39:04 -0400

    feat: crud implements and tests

--- src/main/java/netconnection/app/back/domain/service/ClientServiceImpl.java
+++ src/main/java/netconnection/app/back/domain/service/ClientServiceImpl.java
@@ -1,22 +1,42 @@
 package netconnection.app.back.domain.service;
 
-import netconnection.app.back.application.dtos.ClientDTO;
+import netconnection.app.back.domain.model.Client;
+import netconnection.app.back.domain.port.ClientRepositoryPort;
 import netconnection.app.back.domain.port.ClientServicePort;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClientServiceImpl implements ClientServicePort {
 
 
     ClientServicePort clientServicePort;
+    ClientRepositoryPort clientRepositoryPort;
 
-    public ClientServiceImpl(ClientServicePort _clientServicePort){
+    public ClientServiceImpl(ClientServicePort _clientServicePort, ClientRepositoryPort _clientRepositoryPort){
         this.clientServicePort = _clientServicePort;
+        this.clientRepositoryPort =_clientRepositoryPort;
     }
 
 
     @Override
-    public void saveClient(ClientDTO clientDTO) {
+    public Client save(Client client) {
 
+        clientRepositoryPort.salvar(client);
+        return client;
+
+    }
+
+    @Override
+    public Client findById(String id) {
+
+        return clientRepositoryPort.findById(id);
+    }
+
+    @Override
+    public List<Client> findAll() {
+
+
+        return clientRepositoryPort.findAll();
     }
 }
