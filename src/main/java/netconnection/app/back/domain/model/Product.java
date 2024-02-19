5a2382a25edc0b68a744fcb552d4fda4a1d45f4d
Danubia Macedo <dubamacedoo@gmail.com>
2024-02-19 17:30:07 -0400

    feat; new Product entity

---
+++ src/main/java/netconnection/app/back/domain/model/Product.java
@@ -0,0 +1,51 @@
+package netconnection.app.back.domain.model;
+
+import io.micrometer.common.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Product {
+
+    private String name;
+    private String descrition;
+    private Float value;
+    private String url;
+    private List<String> keyWords;
+
+    public Product(String _name, String _description, Float _value, String _url, List<String> _keywords){
+        this.name = _name;
+        this.descrition = _description;
+        this.value = _value;
+        this.url = _url;
+        this.keyWords = _keywords;
+
+        this.validate();
+    }
+
+    private void validate() {
+
+        if(StringUtils.isEmpty(this.name)){
+            throw new IllegalArgumentException("Name must not be empty");
+        }
+
+        if(StringUtils.isEmpty(this.descrition)){
+            throw new IllegalArgumentException("Description  must not be empty");
+        }
+        if(this.value == null ){
+            throw new IllegalArgumentException("Value  must not  be empty");
+        }
+        if(this.value < 0 ){
+            throw new IllegalArgumentException("Value  must not be negative ");
+        }
+
+        if( StringUtils.isEmpty(this.url)){
+            throw new IllegalArgumentException("URL Product not should be empty");
+        }
+
+
+    }
+
+
+}
