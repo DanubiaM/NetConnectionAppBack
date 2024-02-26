22f214394a063b56ddcdddbd906d5aaa456e533d
Danubia Macedo <dubamacedoo@gmail.com>
2024-02-26 17:10:23 -0400

    feat: implemented notification pattern

---
+++ src/main/java/netconnection/app/back/domain/model/_shared/Notification.java
@@ -0,0 +1,42 @@
+package netconnection.app.back.domain.model._shared;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Notification implements NotificationInterface{
+    private List<Error> errors = new ArrayList<>();
+
+
+    @Override
+    public void addError(String message) {
+        addError(message, null);
+    }
+
+    @Override
+    public void addError(String message, Exception e) {
+        errors.add(new Error(message, e));
+    }
+
+    @Override
+    public boolean hasErrors() {
+        return ! errors.isEmpty();
+    }
+
+    @Override
+    public String errorMessage() {
+        return errors.stream()
+                .map(e -> e.message)
+                .collect(Collectors.joining(", "));
+    }
+
+    private static class Error {
+        String message;
+        Exception cause;
+
+        private Error(String message, Exception cause) {
+            this.message = message;
+            this.cause = cause;
+        }
+    }
+}
